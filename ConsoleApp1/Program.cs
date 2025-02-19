b2a2e438300084a15e8793904ed6f9aada0f2855
Sashakrm <sasakramch@gmail.com>
2025-02-19 09:37:01 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func()
+        {
+            Console.WriteLine("Сколько n элементов? ");
+            int n = int.Parse(Console.ReadLine());
+            Input(n);
+        }
+        static void Input(int n)
+        {
+            int[] mas = new int[n];
+            int i;
+            for (i = 0; i < mas.Length; i++)
+            {
+                Console.Write($"массив[{i}]=");
+                mas[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+            }
+            for (i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine($"массив[{i}]= {mas[i]}");
+            }
+        }
+    }
+}
