2ccae04487a05fc4027df35b82039c49bc2c0265
Sashakrm <sasakramch@gmail.com>
2025-02-19 09:39:11 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func()
+        {
+            int[] mas = new int[5] { 12, 34, 25, 3, 45 };
+            int i;
+            for(i=0;i<mas.Length;i++)
+            {
+                Console.WriteLine($"mas[{i}]= {mas[i]}");
+            }
+        }
+    }
+}
