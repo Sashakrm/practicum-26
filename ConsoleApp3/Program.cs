a77b06c955ed5e2eb3089593e9696100af1044f7
Sashakrm <sasakramch@gmail.com>
2025-02-19 09:38:30 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func()
+        {
+            int[] mas = new int[8];
+            int i;
+            for(i=0;i<mas.Length;i++)
+            {
+                mas[i] = i;
+            }
+            foreach(int element in mas)
+            {
+                Console.Write($"{element}\t");
+            }
+        }
+    }
+}
