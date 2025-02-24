b0c634d14b820889a8802b989730a34b9e6b7525
Sashakrm <sasakramch@gmail.com>
2025-02-24 21:05:43 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Input("n= ");
+            int[] mas = new int[n];
+            Mas(n,mas);
+            End(mas);
+            Console.Read();
+        }
+        static int Input(string mes)
+        {
+            Console.WriteLine(mes);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Mas (int n, int[]mas)
+        {
+            int i;
+            for (i = 0; i < mas.Length; i++)
+            {
+                Console.Write($"массив[{i}]=");
+                mas[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static void End(int[]mas)
+        {
+            int i;
+            for (i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine($"массив[{i}]= {mas[i]}");
+            }
+        }
+    }
+}
